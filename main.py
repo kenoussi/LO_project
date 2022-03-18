eeb4a9121abc672f302c65c2291522cb1045c0ac
ykenouss <ykenouss@stud.hs-offenburg.de>
2022-03-18 16:23:51 +0100

    we hided secret.py

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 
 import numpy as np
 
+
+
+def my_addition_func(i,j):
+    k=j+i
+    return k
+
 a =[4, 5]
 
 print(a)
 a=a*2
 print("Now a is:",a)
 print("i did some changes")
+print("addition of 5 and 9 is:", my_addition_func(5,9))
